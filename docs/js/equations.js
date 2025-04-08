bafdc033bc8ab019f32b528ba2cd3e6ee6457420
oREAGanoWombat <reagan.womack@outlook.com>
2025-04-07 21:59:25 -0400

    added new equations, divided equation card generation into equations, unit-conversions, and constants

--- docs/js/equations.js
+++ docs/js/equations.js
@@ -1,5 +1,7 @@
 const equationsDiv = document.getElementById('equations-placeholder');
 const equationByNameDiv = document.querySelectorAll(".equation-container.by-name");
+const unitConversionsDiv = document.getElementById('unit-conversions-placeholder');
+const constantsDiv = document.getElementById('constants-placeholder');
 // const equationByNameDiv = document.getElementById('equation-by-name');
 
 const equationsArray = {
@@ -24,18 +26,6 @@ const equationsArray = {
             notes: "\\[ \\begin{aligned} m & = \\text{ mass } \\end{aligned} \\]",
             general_chemistry: true,
             organic_chemistry: false,
-        },
-        {
-            name: "ppm",
-            equation: "\\begin{equation} \\text{ppm} = \\frac{g_{solute}}{1 \\times 10^6 g_{solution}} \\end{equation}",
-            notes: "\\[ \\begin{aligned} g_{solute} & = \\text{ mass of solute (g)} \\\\ g_{solution} & = \\text{ mass of solution (g)} \\end{aligned} \\]",
-            general_chemistry: true,
-        },
-        {
-            name: "molality",
-            equation: "\\begin{equation} m = \\frac{n}{\\text{kg solvent}} \\end{equation}",
-            notes: "\\[ \\begin{aligned} m & = \\text{ molality (mol/kg)} \\\\ n & = \\text{ moles of solute} \\\\ \\text{kg solvent} & = \\text{ mass of solvent (kg)} \\end{aligned} \\]",
-            general_chemistry: true,
         },
         {
             name: "percent-volume",
@@ -80,31 +70,11 @@ const equationsArray = {
             general_chemistry: true,
         },
         {
-            name: "gas-laws",
-            equation: "\\begin{equation} 1 \\text{ atm} = 760 \\text{ mmHg} = 760 \\text{ torr} \\end{equation}",
-            notes: null,
-            unit_conversion: true,
-            general_chemistry: false,
-        },
-        {
-            name: "ideal-gas-constant",
-            equation: "\\begin{equation} R = 0.0821 \\frac{L \\cdot atm}{K \\cdot mol} = 8.314 \\frac{J}{K \\cdot mol} \\end{equation}",
-            notes: null,
-            constant: true,
-            general_chemistry: false,
-        },
-        {
-            name: "kelvin-conversion",
-            equation: "\\begin{equation} T(K) = T(^{\\circ}C) + 273.15 \\end{equation}",
-            notes: null,
-            unit_conversion: true,
-            general_chemistry: false,
-        },
-        {
             name: "ideal-gas-law",
             equation: "\\begin{equation} PV = nRT \\end{equation}",
             notes: "\\[ \\begin{aligned} P & = \\text{ pressure (atm)} \\\\ V & = \\text{ volume (L)} \\\\ n & = \\text{ moles of gas} \\\\ R & = \\text{ ideal gas constant} \\\\ T & = \\text{ temperature (K)} \\end{aligned} \\]",
             general_chemistry: true,
+            exam2: true;
         },
         {
             name: "energy-equations",
@@ -129,6 +99,7 @@ const equationsArray = {
             equation: "\\begin{equation} \\begin{aligned} \\Delta H &= \\sum \\text{coeff} \\cdot \\Delta H_f^{\\circ} (\\text{products}) \\\\ &\\quad - \\sum \\text{coeff} \\cdot \\Delta H_f^{\\circ} (\\text{reactants}) \\end{aligned} \\end{equation}",
             notes: "\\[ \\begin{aligned} \\Delta H & = \\text{ enthalpy change of reaction} \\\\ \\Delta H_f^{\\circ} & = \\text{ standard enthalpy of formation} \\\\ \\text{coeff} & = \\text{ stoichiometric coefficients from balanced equation} \\end{aligned} \\]",
             general_chemistry: true,
+            exam2: true;
         },
         {
             name: "standard-entropy-change",
@@ -144,27 +115,45 @@ const equationsArray = {
         },
         {
             name: "gibbs-free-energy-B",
-            equation: "\\begin{equation} \\Delta G = \\Delta G^{\\circ} + RT \\ \\ln Q \\end{equation}",
+            equation: "\\begin{equation} \\Delta G = \\Delta G^{\\circ} + RT \\ \\ln Q = RT \\ln \\left(\\frac{Q}{K}\\right) \\end{equation}",
             notes: "\\( Q = \\text{ reaction quotient} \\)",
             general_chemistry: true,
+            exam2: true;
         },
         {
             name: "standard-gibbs-free-energy",
             equation: "\\begin{equation} \\Delta G^{\\circ} = -RT \\ \\ln K \\end{equation}",
             notes: "\\( \\begin{aligned} R & = \\text{ universal gas constant} \\\\ T & = \\text{ temperature (K)} \\\\ K & = \\text{ equilibrium constant} \\end{aligned} \\)",
             general_chemistry: true,
+            exam2: true;
+        },
+        {
+            name: "standard-gibbs-free-energy-2",
+            equation: "\\begin{equation} \\Delta G^{\\circ} = \\Delta H^{\\circ} - T \\Delta S^{\\circ} \\end{equation}",
+            notes: "",
+            general_chemistry: true,
+            exam2: true;
         },
         {
             name: "equilibrium-constant-concentrations",
             equation: "\\begin{equation} K_{C} = \\frac{[C]^{c} [D]^{d}}{[A]^{a} [B]^{b}} \\end{equation}",
             notes: "",
             general_chemistry: true,
+            exam2: true;
         },
         {
             name: "equilibrium-constant-pressures",
-            equation: "\\begin{equation} K_{P} = \\frac{P_{C}^{c} P_{D}^{d}}{P_{A}^{a} P_{B}^{b}} \\end{equation}",
+            equation: "\\begin{equation} K_{p} = \\frac{P_{C}^{c} P_{D}^{d}}{P_{A}^{a} P_{B}^{b}} \\end{equation}",
             notes: "",
             general_chemistry: true,
+            exam2: true;
+        },
+        {
+            name: "equilibrium-constant-conversion",
+            equation: "\\begin{equation} K_{p} = K_{c} (RT)^{ \\Delta n_{gas}} \\end{equation}",
+            notes: "",
+            general_chemistry: true,
+            exam2: true;
         },
         {
             name: "entropy-vaporization",
@@ -183,7 +172,128 @@ const equationsArray = {
             equation: "\\begin{equation} Q_{P} = \\frac{P_{C}^{c} P_{D}^{d}}{P_{A}^{a} P_{B}^{b}} \\end{equation}",
             notes: "",
             general_chemistry: true,
+        },
+        {
+            name: "heat-bomb-calorimeter",
+            equation: "\\begin{equation} q_{cal} = -C_{cal} \\ \\Delta T_{cal} = \\Delta E \\end{equation}",
+            notes: "",
+            general_chemistry: true,
+            exam2: true;
+        },
+        {
+            name: "second-equilibrium-constant",
+            equation: "\\begin{equation} \\ln K_{2} = \\ln K_{1} + \\left(\\frac{\\Delta H^{\\circ}}{R}\\right) \\left[ \\left(\\frac{1}{T_{1}}\\right) - \\left(\\frac{1}{T_{2}}\\right) \\right] \\end{equation}",
+            notes: "",
+            general_chemistry: true,
+            exam2: true;
+        },
+        {
+            name: "pH",
+            equation: "\\begin{equation} \\begin{aligned} pH & = -\\log_{10}\\left[\\ce{ H3O+ }\\right] \\\\ \\left[\\ce{ H3O+ }\\right] & = 10^{-pH} \\\\ \\left[\\ce{ H3O+ }\\right] & = \\sqrt{K_{a} \\times c_{0}} \\end{aligned} \\end{equation}",
+            notes: "",
+            general_chemistry: true,
+            exam2: true;
+        },
+        {
+            name: "pOH",
+            equation: "\\begin{equation} \\begin{aligned} pOH & = -\\log_{10}\\left[\\ce{ OH- }\\right] \\\\ \\left[\\ce{ OH- }\\right] & = 10^{-pOH} \\\\ \\left[\\ce{ OH- }\\right] & = \\sqrt{K_{b} \\times c_{0}} \\end{aligned} \\end{equation}",
+            notes: "",
+            general_chemistry: true,
+            exam2: true;
+        },
+        {
+            name: "pH-pOH-relation",
+            equation: "\\begin{equation} \\begin{aligned} pH + pOH & = 14 \\\\ K_{w} & = \\left[\\ce{ H3O+ }\\right] \\left[\\ce{ OH- }\\right] \\\\ & = 1.0*10^{-14} \\end{aligned} \\end{equation}",
+            notes: "\\begin{aligned} T = 298.15 K \\end{aligned}",
+            general_chemistry: true,
+            exam2: true;
+        },
+        {
+            name: "pH-polyprotic",
+            equation: "\\begin{equation} pH\\left(\\text{polyprotic acid}\\right) = \\frac{\\left(pK_{a1} + pK_{a2}\\right)}{2} \\end{equation}",
+            notes: "",
+            general_chemistry: true,
+            exam2: true;
+        },
+        {
+            name: "pKa",
+            equation: "\\begin{equation} \\begin{aligned} pK_{a} & = -\\log_{10}\\left(K_{a}\\right) \\\\ K_{a} & = 10^{-pK_{a}} \\end{aligned} \\end{equation}",
+            notes: "",
+            general_chemistry: true,
+            exam2: true;
+        },
+        {
+            name: "pKb",
+            equation: "\\begin{equation} \\begin{aligned} pK_{b} & = -\\log_{10}\\left(K_{b}\\right) \\\\ K_{b} & = 10^{-pK_{b}} \\end{aligned} \\end{equation}",
+            notes: "",
+            general_chemistry: true,
+            exam2: true;
+        },
+        {
+            name: "pKa-pKb-relation",
+            equation: "\\begin{equation} \\begin{aligned} pK_{a} + pK_{b} & = 14 \\\\ K_{w} & = K_{a} * K_{b} \\end{aligned} \\end{equation}",
+            notes: "\\begin{aligned} T = 298.15 K \\end{aligned}",
+            general_chemistry: true,
+            exam2: true;
+        },
+        {
+            name: "quadratic-formula",
+            equation: "\\begin{equation} x = \\frac{-b\\pm\\sqrt{b^{2}-4ac}}{2a} \\end{equation}",
+            notes: "",
+            general_chemistry: true,
+            exam2: true;
+        },
+        {
+            name: "percent-ionization-estimate",
+            equation: "\\begin{equation} \\text{% ionization estimate} = \\sqrt{\\frac{K_{a,b}}{c_{0}}} \\times 100\\% \\end{equation}",
+            notes: "",
+            general_chemistry: true,
+            exam2: true;
+        },
+        {
+            name: "equilibrium-constant-reaction",
+            equation: "\\begin{equation} K_{reaction} = \\frac{K_{a}\\left(\\text{forward reacting acid}\\right)}{K_{a}\\left(\\text{reverse reacting/produced acid}\\right)} \\end{equation}",
+            notes: "",
+            general_chemistry: true,
+            exam2: true;
         }
+    ],
+    unitConversions: [
+        {
+            name: "ppm",
+            equation: "\\begin{equation} \\text{ppm} = \\frac{g_{solute}}{1 \\times 10^6 g_{solution}} \\end{equation}",
+            notes: "\\[ \\begin{aligned} g_{solute} & = \\text{ mass of solute (g)} \\\\ g_{solution} & = \\text{ mass of solution (g)} \\end{aligned} \\]",
+            general_chemistry: true,
+        },
+        {
+            name: "molality",
+            equation: "\\begin{equation} m = \\frac{n}{\\text{kg solvent}} \\end{equation}",
+            notes: "\\[ \\begin{aligned} m & = \\text{ molality (mol/kg)} \\\\ n & = \\text{ moles of solute} \\\\ \\text{kg solvent} & = \\text{ mass of solvent (kg)} \\end{aligned} \\]",
+            general_chemistry: true,
+        },
+        {
+            name: "pressure-units",
+            equation: "\\begin{equation} 1 \\text{ atm} = 760 \\text{ mmHg} = 760 \\text{ torr} \\end{equation}",
+            notes: null,
+            general_chemistry: true,
+            exam2: true;
+        },
+        {
+            name: "kelvin-conversion",
+            equation: "\\begin{equation} T(K) = T(^{\\circ}C) + 273.15 \\end{equation}",
+            notes: null,
+            general_chemistry: true,
+            exam2: true;
+        },
+    ],
+    constants: [
+        {
+            name: "ideal-gas-constant",
+            equation: "\\begin{equation} R = 0.0821 \\frac{L \\cdot atm}{K \\cdot mol} = 8.314 \\frac{J}{K \\cdot mol} \\end{equation}",
+            notes: null,
+            general_chemistry: true,
+            exam2: true;
+        },
     ]
 };
 
@@ -210,10 +320,56 @@ const setEquationCards = (arr = equationsArray.equations) => {
         </div>
     `).join("");
 
-    // Re-render MathJax to ensure equations are processed
-    if (window.MathJax) {
-        MathJax.typesetPromise();
+};
+
+const setUnitConversionCards = (arr = equationsArray.unitConversions) => {
+    //Determine discipline based on #unit-conversions-placeholder
+    let discipline = null;
+    if (unitConversionsDiv == null) {
+        return;
     }
+    if (unitConversionsDiv.classList.contains("generalChemistry")) {
+        discipline = "general_chemistry";
+    } else if (unitConversionsDiv.classList.contains("organicChemistry")) {
+        discipline = "organic_chemistry";
+    }
+
+    // Filter equations based on div class
+    const filteredEquations = arr.filter(eq => eq[discipline]);
+
+    // Generate equation cards
+    unitConversionsDiv.innerHTML += filteredEquations.map(({ name, equation, notes }) => `
+        <div class="equation-card">
+            ${equation} 
+            <p><span class="equation-notes">${notes !== null ? notes : ""}</span></p>
+        </div>
+    `).join("");
+
+};
+
+const setConstantsCards = (arr = equationsArray.constants) => {
+    //Determine discipline based on #constants-placeholder
+    let discipline = null;
+    if (constantsDiv == null) {
+        return;
+    }
+    if (constantsDiv.classList.contains("generalChemistry")) {
+        discipline = "general_chemistry";
+    } else if (constantsDiv.classList.contains("organicChemistry")) {
+        discipline = "organic_chemistry";
+    }
+
+    // Filter equations based on div class
+    const filteredEquations = arr.filter(eq => eq[discipline]);
+
+    // Generate equation cards
+    constantsDiv.innerHTML += filteredEquations.map(({ name, equation, notes }) => `
+        <div class="equation-card">
+            ${equation} 
+            <p><span class="equation-notes">${notes !== null ? notes : ""}</span></p>
+        </div>
+    `).join("");
+
 };
 
 const setEquationByName = () => {
@@ -241,4 +397,6 @@ const setEquationByName = () => {
 
 // run functions when page loads
 setEquationCards();
+setUnitConversionCards();
+setConstantsCards();
 setEquationByName();
